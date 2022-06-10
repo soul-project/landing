44e8fd6011da5fb73f67b25768eccdb680c93747
lws803 <lws803@gmail.com>
2022-06-10 22:00:14 +0800

    integrate chakra components

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Box backgroundColor="black" height="100px" margin={0} />
+    </footer>
+  );
+}
---
+++ theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    soul: {
+      pink: { light: "#F72585", dark: "#B5179E", 200: "#F72585" },
+      green: { 200: "#02DE7B" },
+    },
+  },
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "#141516",
+      },
+    },
+  },
+});
+
+export default theme;
