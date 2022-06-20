db258e7ae0ce72781a656a7937997eb3ded80f10
Wilson Ler <lws803@gmail.com>
2022-06-21 01:24:17 +0800

    Fix refresh token not refreshing (#8)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
 import theme from "src/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   );
 }
 
