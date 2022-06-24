937562548488021cf0cea075839db235c6eaac48
Wilson Ler <lws803@gmail.com>
2022-06-24 19:43:18 +0800

    Added monitoring and fixing refresh token flow (#11)
    * added sentry monitoring for refresh token issues
    
    * fixed linting issue
    
    * committing Sentry DSN to public repo
    
    * build with sentry auth token
    
    * added auth token
    
    * added build args to sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = nextConfig
+const moduleExports = () => {
+  const nextConfig = {
+    reactStrictMode: true,
+  };
+  return nextConfig;
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
