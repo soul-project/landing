82c9701f520890259a0eac3bd9b42f716bbe36ca
lws803 <lws803@gmail.com>
2022-07-24 17:10:17 +0800

    refactored out components

--- src/components/my-platforms/AddNewPlatformModal.tsx
+++ src/components/my-platforms/AddNewPlatformModal.tsx
@@ -8,20 +8,13 @@ import {
   ModalBody,
   Button,
   ModalFooter,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
   VStack,
-  InputGroup,
-  InputLeftElement,
-  HStack,
-  InputRightElement,
-  Box,
 } from "@chakra-ui/react";
-import { Formik, Form, Field, FieldProps, FieldArray } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import { SmallCloseIcon } from "@chakra-ui/icons";
+
+import RedirectUrisField from "./AddNewPlatformModal/RedirectUrisField";
+import PlatformNameField from "./AddNewPlatformModal/PlatformNameField";
 
 export default function AddNewPlatformModal({ isOpen, onClose }: Props) {
   return (
@@ -46,94 +39,8 @@ export default function AddNewPlatformModal({ isOpen, onClose }: Props) {
             <Form>
               <ModalBody>
                 <VStack spacing="16px" alignItems="flex-start">
-                  <Field name="name">
-                    {({ field, form }: FieldProps<string>) => (
-                      <FormControl
-                        isInvalid={!!form.errors.name && !!form.touched.name}
-                      >
-                        <FormLabel>Name</FormLabel>
-                        <Input
-                          {...field}
-                          placeholder="enter your platform name"
-                          variant="filled"
-                        />
-                        {form.errors.name && (
-                          <FormErrorMessage>
-                            {String(form.errors.name)}
-                          </FormErrorMessage>
-                        )}
-                      </FormControl>
-                    )}
-                  </Field>
-                  {/* TODO: Refactor and move this out into a separate component */}
-                  <FieldArray name="redirectUris">
-                    {({ remove, push }) => (
-                      <>
-                        <FormLabel>Redirect Uris</FormLabel>
-                        {values.redirectUris.map((_redirectUri, index) => (
-                          <Field key={index} name={`redirectUris.${index}`}>
-                            {({
-                              field,
-                              form,
-                            }: FieldProps<
-                              string,
-                              { redirectUris: string[] }
-                            >) => {
-                              const errors = form.errors.redirectUris as
-                                | string[]
-                                | undefined;
-                              const touched = form.touched.redirectUris as
-                                | boolean[]
-                                | undefined;
-                              return (
-                                <FormControl
-                                  isInvalid={
-                                    !!errors?.[index] && !!touched?.[index]
-                                  }
-                                >
-                                  <HStack>
-                                    <InputGroup>
-                                      <InputLeftElement>
-                                        {index + 1}
-                                      </InputLeftElement>
-                                      <Input
-                                        {...field}
-                                        placeholder="enter a redirect uri"
-                                        variant="filled"
-                                      />
-                                      {index > 0 && (
-                                        <InputRightElement>
-                                          <Box
-                                            as="button"
-                                            width="100%"
-                                            height="100%"
-                                          >
-                                            <SmallCloseIcon
-                                              onClick={() => remove(index)}
-                                            />
-                                          </Box>
-                                        </InputRightElement>
-                                      )}
-                                    </InputGroup>
-                                  </HStack>
-                                  {errors?.[index] && (
-                                    <FormErrorMessage>
-                                      {errors?.[index]}
-                                    </FormErrorMessage>
-                                  )}
-                                </FormControl>
-                              );
-                            }}
-                          </Field>
-                        ))}
-                        {values.redirectUris.length < 5 && (
-                          <Button onClick={() => push("")}>
-                            Add more redirect uris
-                          </Button>
-                        )}
-                      </>
-                    )}
-                  </FieldArray>
+                  <PlatformNameField />
+                  <RedirectUrisField redirectUris={values.redirectUris} />
                 </VStack>
               </ModalBody>
               <ModalFooter>
---
+++ src/components/my-platforms/AddNewPlatformModal/RedirectUrisField.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { SmallCloseIcon } from "@chakra-ui/icons";
+import {
+  FormLabel,
+  FormControl,
+  HStack,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Box,
+  FormErrorMessage,
+  Button,
+} from "@chakra-ui/react";
+import { FieldArray, Field, FieldProps } from "formik";
+
+export default function RedirectUrisField({ redirectUris }: Props) {
+  return (
+    <FieldArray name="redirectUris">
+      {({ remove, push }) => (
+        <>
+          <FormLabel>Redirect Uris</FormLabel>
+          {redirectUris.map((_redirectUri, index) => (
+            <Field key={index} name={`redirectUris.${index}`}>
+              {({
+                field,
+                form,
+              }: FieldProps<string, { redirectUris: string[] }>) => {
+                const errors = form.errors.redirectUris as string[] | undefined;
+                const touched = form.touched.redirectUris as
+                  | boolean[]
+                  | undefined;
+                return (
+                  <FormControl
+                    isInvalid={!!errors?.[index] && !!touched?.[index]}
+                  >
+                    <HStack>
+                      <InputGroup>
+                        <InputLeftElement>{index + 1}</InputLeftElement>
+                        <Input
+                          {...field}
+                          placeholder="enter a redirect uri"
+                          variant="filled"
+                        />
+                        {index > 0 && (
+                          <InputRightElement>
+                            <Box as="button" width="100%" height="100%">
+                              <SmallCloseIcon onClick={() => remove(index)} />
+                            </Box>
+                          </InputRightElement>
+                        )}
+                      </InputGroup>
+                    </HStack>
+                    {errors?.[index] && (
+                      <FormErrorMessage>{errors?.[index]}</FormErrorMessage>
+                    )}
+                  </FormControl>
+                );
+              }}
+            </Field>
+          ))}
+          {redirectUris.length < 5 && (
+            <Button onClick={() => push("")}>Add more redirect uris</Button>
+          )}
+        </>
+      )}
+    </FieldArray>
+  );
+}
+
+type Props = {
+  redirectUris: string[];
+};
