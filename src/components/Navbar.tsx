cbd68bacedef35b1e429e3ba69581fd8757de79c
lws803 <lws803@gmail.com>
2022-08-12 00:31:46 +0800

    re-added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import NextLink from "next/link";
+import { Button, HStack, Link } from "@chakra-ui/react";
+
+export default function Navbar({ onSignIn, isSignedIn, onSignOut }: Props) {
+  return (
+    <HStack
+      justifyContent="space-between"
+      padding="32px 0px"
+      position={["initial", "initial", "initial", "sticky", "sticky"]}
+      top="0px"
+      bgColor="soul.bgGrey"
+      zIndex={1}
+    >
+      <HStack spacing="16px">
+        <NextLink passHref href="/">
+          <Link>Home</Link>
+        </NextLink>
+
+        {isSignedIn && (
+          <NextLink passHref href="/my-platforms">
+            <Link>Platforms</Link>
+          </NextLink>
+        )}
+
+        {/* TODO: Hidden for the time being because we don't have flushed out docs yet */}
+        <NextLink passHref href="/docs/introduction">
+          <Link>Docs</Link>
+        </NextLink>
+      </HStack>
+      <Button
+        variant="outline"
+        onClick={() => (isSignedIn ? onSignOut() : onSignIn())}
+      >
+        {isSignedIn ? "Logout" : "Login"}
+      </Button>
+    </HStack>
+  );
+}
+
+type Props = {
+  onSignIn: () => void;
+  onSignOut: () => void;
+  isSignedIn: boolean;
+};
