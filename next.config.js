a65542684fa3f76695dc085a4ff45fe81c1409ce
lws803 <lws803@gmail.com>
2022-09-03 11:50:09 +0800

    use public env variables

--- next.config.js
+++ next.config.js
@@ -9,19 +9,13 @@ const moduleExports = () => {
   return nextConfig;
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+const sentryWebpackPluginOptions = { silent: true };
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(
-  withContentlayer(moduleExports),
-  sentryWebpackPluginOptions
-);
+if (process.env.APP_ENV === "production") {
+  module.exports = withSentryConfig(
+    withContentlayer(moduleExports),
+    sentryWebpackPluginOptions
+  );
+} else {
+  module.exports = withContentlayer(moduleExports);
+}
