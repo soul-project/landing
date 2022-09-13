a4c5fa4d80946aa434822c79d3e0e7301e1b538c
lws803 <lws803@gmail.com>
2022-09-13 22:07:39 +0800

    check to make sure key exists first

--- src/pages/api/utils/firebase-admin.ts
+++ src/pages/api/utils/firebase-admin.ts
@@ -7,7 +7,9 @@ export function getFirebaseAdminApp() {
   } catch (error) {
     firebaseAdminApp = initializeApp({
       credential: cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        privateKey:
+          process.env.FIREBASE_PRIVATE_KEY &&
+          process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       }),
