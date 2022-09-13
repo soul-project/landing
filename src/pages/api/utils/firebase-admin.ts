c2b37489db2a818773fe6610e33c6f1544439aa9
lws803 <lws803@gmail.com>
2022-09-13 22:06:02 +0800

    escape private keys

--- src/pages/api/utils/firebase-admin.ts
+++ src/pages/api/utils/firebase-admin.ts
@@ -1,18 +1,13 @@
-/* eslint-disable no-console */
 import { cert, initializeApp, getApp } from "firebase-admin/app";
 
 export function getFirebaseAdminApp() {
-  console.log(process.env.FIREBASE_PRIVATE_KEY);
-  console.log(process.env.FIREBASE_PROJECT_ID);
-  console.log(process.env.FIREBASE_CLIENT_EMAIL);
-
   let firebaseAdminApp;
   try {
     firebaseAdminApp = getApp();
   } catch (error) {
     firebaseAdminApp = initializeApp({
       credential: cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       }),
