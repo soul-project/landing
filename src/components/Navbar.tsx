f5fac37e86c5cb66ba01c25b2322f977fb4c0d4e
Wilson Ler <lws803@gmail.com>
2022-09-18 23:41:06 +0800

    Add access token at top nav bar (#66)
    * add access token at top nav bar
    
    * green tick when copy
    
    * randomize login emoji

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,82 @@
 import NextLink from "next/link";
-import { Button, HStack, Link } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  IconButton,
+  Link,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { LockIcon } from "@chakra-ui/icons";
 
-export default function Navbar() {
+import AccessTokenModal from "./index/AccessTokenModal";
+
+export default function Navbar({ hideAccessTokenButton }: Props) {
   const { data: session, status } = useSession();
-  return (
-    <HStack
-      justifyContent="space-between"
-      padding="32px 0px"
-      position={["initial", "initial", "initial", "sticky", "sticky"]}
-      top="0px"
-      bgColor="soul.bgGrey"
-      zIndex={1}
-    >
-      <HStack spacing="16px">
-        <NextLink passHref href="/">
-          <Link>Home</Link>
-        </NextLink>
+  const {
+    isOpen: isAccessTokenModalOpen,
+    onOpen: onOpenAccessTokenModal,
+    onClose: onCloseAccessTokenModal,
+  } = useDisclosure();
 
-        <NextLink passHref href="/docs/getting-started">
-          <Link>Docs</Link>
-        </NextLink>
-      </HStack>
-      <Button
-        variant="outline"
-        onClick={() => (!!session ? signOut() : signIn("soul"))}
-        isLoading={status === "loading"}
+  return (
+    <>
+      <AccessTokenModal
+        isOpen={isAccessTokenModalOpen}
+        onClose={onCloseAccessTokenModal}
+      />
+
+      <HStack
+        justifyContent="space-between"
+        padding="32px 0px"
+        position={["initial", "initial", "initial", "sticky", "sticky"]}
+        top="0px"
+        bgColor="soul.bgGrey"
+        zIndex={1}
       >
-        {!!session ? "Logout" : "Login"}
-      </Button>
-    </HStack>
+        <HStack spacing="16px">
+          <NextLink passHref href="/">
+            <Link>Home</Link>
+          </NextLink>
+
+          <NextLink passHref href="/docs/getting-started">
+            <Link>Docs</Link>
+          </NextLink>
+        </HStack>
+        <HStack>
+          {!!session && !hideAccessTokenButton && (
+            <>
+              <IconButton
+                variant="outline"
+                icon={<LockIcon />}
+                aria-label="Open access token modal"
+                onClick={onOpenAccessTokenModal}
+                display={["block", "block", "none"]}
+              />
+              <Button
+                variant="outline"
+                leftIcon={<LockIcon />}
+                onClick={onOpenAccessTokenModal}
+                display={["none", "none", "block"]}
+              >
+                Access token
+              </Button>
+            </>
+          )}
+
+          <Button
+            variant="outline"
+            onClick={() => (!!session ? signOut() : signIn("soul"))}
+            isLoading={status === "loading"}
+          >
+            {!!session ? "Logout" : "Login"}
+          </Button>
+        </HStack>
+      </HStack>
+    </>
   );
 }
+
+type Props = {
+  hideAccessTokenButton?: boolean;
+};
